4483191e9b18ff21f5139de6584b3ddfbe0d039f
Steve Rivera <srivera.x1@gmail.com>
2020-02-16 23:51:44 -0500

    Updates

---
+++ precache-manifest.a6451102b74b21afad5f9ab28bd68aba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15dec567e0074789395239ed68b21962",
+    "url": "/unclemark/index.html"
+  },
+  {
+    "revision": "409b1f01423f8b3f77a6",
+    "url": "/unclemark/static/css/main.2624c0b8.chunk.css"
+  },
+  {
+    "revision": "52a2d49de1aa3683741a",
+    "url": "/unclemark/static/js/2.1fffb467.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/unclemark/static/js/2.1fffb467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "409b1f01423f8b3f77a6",
+    "url": "/unclemark/static/js/main.bd1135de.chunk.js"
+  },
+  {
+    "revision": "7ebc0d113cecec1c8000",
+    "url": "/unclemark/static/js/runtime-main.ad00f579.js"
+  },
+  {
+    "revision": "81b7e85b0bd3a2a28a049c2a6b63087d",
+    "url": "/unclemark/static/media/unclemark.81b7e85b.jpg"
+  }
+]);
